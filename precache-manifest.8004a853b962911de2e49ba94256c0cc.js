1078401e7b13150fd952d0d857086d77f652810a
ChrisPayne <chris.payne93@hotmail.com>
2019-08-28 12:14:48 +0100

    Updates

---
+++ precache-manifest.8004a853b962911de2e49ba94256c0cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db630077dda007e5397273d5df7bcc33",
+    "url": "/monsterRolo/index.html"
+  },
+  {
+    "revision": "07302f45a943ff86cb3a",
+    "url": "/monsterRolo/static/css/main.538d448f.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsterRolo/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "07302f45a943ff86cb3a",
+    "url": "/monsterRolo/static/js/main.b35ab9c3.chunk.js"
+  },
+  {
+    "revision": "fc0d64546a5e5e4fa61d",
+    "url": "/monsterRolo/static/js/runtime~main.778eb95b.js"
+  }
+]);
